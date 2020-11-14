5bc6e60926df019ec6f0f4620eb7f8e2800ef61a
Borovikov <borovikovvaleksey@gmail.com>
2020-11-14 18:07:00 +0200

    install express

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-const express = require("express");
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Hello from server side', app: "natours"});
+})
+
+const port = 2000;
+
+app.listen(port, () => {
+    console.log(`App run to port ${port}...`);
+});
