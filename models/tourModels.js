5536d76bc253957773e22e87ed0694460d1c3972
Borovikov <borovikovvaleksey@gmail.com>
2021-01-09 00:56:57 +0200

    document and query middleware

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -6,6 +7,7 @@ const tourSchema = new mongoose.Schema({
         require: [true, 'A tour has have a name'],
         unique: true
     },
+    slug: String,
     duration: {
         type: Number,
         require: [true, 'A tour must have a durations']
@@ -50,16 +52,29 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
+    secretTour: {
+        type: Boolean,
+        default: false
+    },
     startDates: [Date]
 },
- {
+{
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
-}
-)
+})
 
 tourSchema.virtual('durationWeek').get(function() {
     return this.duration / 7;
+})
+
+tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, {lower: true});
+    next();
+})
+
+tourSchema.pre( /^find/, function(next) {
+    this.find({ secretTour: { $ne: true }});
+    next();
 })
 
 const Tour = mongoose.model('Tour', tourSchema);
