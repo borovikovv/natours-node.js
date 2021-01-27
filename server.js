d0925e4abd4e44e2b8638e3a35ccb5b3a66fcee0
Borovikov <borovikovvaleksey@gmail.com>
2021-01-27 19:25:09 +0200

    uncaughtRejection

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env'});
+
+process.on('uncaughtRejection', err => {
+    console.log(err.name, err.message)
+    console.log('UNCAUGHT REJECTION!')
+    process.exit(1);
+})
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
