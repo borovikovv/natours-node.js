8939acaba9fd0e8b89b1806831dc4aa4dde6f1f4
Borovikov <borovikovvaleksey@gmail.com>
2021-01-30 23:54:11 +0200

    add token to user

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,13 +1,41 @@
 const User = require('./../models/userModels');
+const jwt = require('jsonwebtoken');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('../utils/AppErrors');
 
 exports.signup = catchAsync( async (req, res, next) => {
-    const newUser = await User.create(req.body);
+
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
 
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser
         }
     })
 })
+
+exports.login = (req, res, next) => {
+    const { email, password } = req.body;
+
+    if(!email && !password) {
+        next(new AppError('Please provide email and password!', 400))
+    }
+
+    const token = "jfldkjfls"
+
+    res.status(200).json({
+        status: "success",
+        token
+    })
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ const authController = require('./../controllers/authControllers');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 
 module.exports = router;
