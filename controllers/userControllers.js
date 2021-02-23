a360d7b28930d9eb8f362be3502f4d2ba57135a9
Borovikov <borovikovvaleksey@gmail.com>
2021-02-23 12:00:20 +0200

    update user data

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,34 @@
 const User = require('./../models/userModels');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/AppErrors');
+
+const filteredObject = (obj, ...allowedFields) => {
+    const newObj = {};
+
+    Object.keys(obj).forEach(el => {
+        if(allowedFields.includes(el)) newObj[el] = obj[el];
+    })
+
+    return newObj;
+};
+
+exports.updateMe = catchAsync( async (req, res, next) => {
+
+    if(req.body.password || req.body.passwordConfirm) {
+        return next(new AppError('This routes is not password updated, please use /updateMyPassword', 400));
+    }
+
+    const filterdBody = filteredObject(req.body, "email", "name");
+    const updateUser = await User.findByIdAndUpdate(req.user.id, filterdBody, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        status: 'success',
+        data: updateUser
+    })
+});
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
     const users = await User.find();
